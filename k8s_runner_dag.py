5905376a734613cba4e160aa23dcc6a7004f9f37
Hugo Fan <hugo.fan@merquri.io>
2025-10-21 12:06:14 +0800

    Fix error

--- k8s_runner_dag.py
+++ k8s_runner_dag.py
@@ -5,29 +5,28 @@ from kubernetes.client import V1Volume, V1VolumeMount, V1ConfigMapVolumeSource
 
 @task
 def run_hello_world_in_k8s():
-    # Define volume from ConfigMap
     volume = V1Volume(
         name="hello-src-volume",
         config_map=V1ConfigMapVolumeSource(name="hello-src")
     )
 
-    # Define mount path inside pod
     volume_mount = V1VolumeMount(
         name="hello-src-volume",
         mount_path="/scripts"
     )
 
     hello_pod = KubernetesPodOperator(
-        task_id="hello_world_pod",
-        name="hello-world-pod",
-        namespace="default",
-        image="python:3.12-slim",
-        cmds=["python", "/scripts/hello.py"],
-        volumes=[volume],
-        volume_mounts=[volume_mount],
-        is_delete_operator_pod=True,
-        get_logs=True,
-    )
+            task_id="run-hello-world",
+            name="hello-world",
+            full_pod_name=True,
+            namespace="default",
+            image="python:3.12-slim",
+            cmds=["python", "/scripts/hello.py"],
+            volumes=[volume],
+            volume_mounts=[volume_mount],
+            is_delete_operator_pod=True,
+            get_logs=True,
+        )
 
     return hello_pod.execute(context={})
 
