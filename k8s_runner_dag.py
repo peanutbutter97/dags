8cc92a4f393b348d699467201382a02f76a2f791
Hugo Fan <hugo.fan@merquri.io>
2025-10-21 11:56:21 +0800

    Fix dag to use V1Volume

--- k8s_runner_dag.py
+++ k8s_runner_dag.py
@@ -1,27 +1,34 @@
 from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import V1Volume, V1VolumeMount, V1ConfigMapVolumeSource
 
 @task
 def run_hello_world_in_k8s():
+    # Define volume from ConfigMap
+    volume = V1Volume(
+        name="hello-src-volume",
+        config_map=V1ConfigMapVolumeSource(name="hello-src")
+    )
+
+    # Define mount path inside pod
+    volume_mount = V1VolumeMount(
+        name="hello-src-volume",
+        mount_path="/scripts"
+    )
+
     hello_pod = KubernetesPodOperator(
         task_id="hello_world_pod",
         name="hello-world-pod",
         namespace="default",
         image="python:3.12-slim",
         cmds=["python", "/scripts/hello.py"],
-        volumes=[{
-            "name": "hello-src-volume",
-            "configMap": {"name": "hello-src"}
-        }],
-        volume_mounts=[{
-            "name": "hello-src-volume",
-            "mountPath": "/scripts"
-        }],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
         is_delete_operator_pod=True,
         get_logs=True,
     )
-    # Execute the pod when the task runs
+
     return hello_pod.execute(context={})
 
 @dag(
